01d405af1b6d5f73d010100a6ec8693fad821d20
osas <osasmortal@gmail.com>
2025-12-14 12:59:31 +0300

    LogManager implemented and integrated into CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/furkan/Desktop/MSH_M5
+# Build directory: /home/furkan/Desktop/MSH_M5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
