68c796e707a1550436828bc42493ab6ab60f033a
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-19 17:00:00 +0300

    Removed build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/furkan/Desktop/MSH_M5
-# Build directory: /home/furkan/Desktop/MSH_M5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
